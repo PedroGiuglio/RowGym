b151e5f37e7c96a273ac41d53f7042e6208f3d39
PedroGiuglio <pedrogiuglio@gmail.com>
2022-08-15 21:14:39 -0300

    fourth commit

---
+++ main.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {myFunction()};
+
+let navbar = document.querySelector("header");
+
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
