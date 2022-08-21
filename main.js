52069444150641595603c2f8ac051c18a53c0db7
PedroGiuglio <pedrogiuglio@gmail.com>
2022-08-21 19:14:12 -0300

    fix bug responsive

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 window.onscroll = function() {myFunction()};
 
 let navbar = document.querySelector("header");
+let fondo = document.querySelector(".tapaFondo");
 
 let sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+    fondo.style.display = "flex";
+
+
   } else {
     navbar.classList.remove("sticky");
   }
